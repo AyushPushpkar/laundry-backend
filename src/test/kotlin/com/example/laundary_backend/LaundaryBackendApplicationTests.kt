db30c39ed1ea66746b9fdea19eefbdc8820d43a9
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-03-18 21:42:55 +0530

    app kt

---
+++ src/test/kotlin/com/example/laundary_backend/LaundaryBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.laundary_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LaundaryBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
